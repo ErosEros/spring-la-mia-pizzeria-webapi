eaf4c7f6aa5b306800504ad9b79a72ab53431924
ErosEros <eros.vittori1994@gmail.com>
2023-02-15 17:52:32 +0100

    completed filter for pizza name + details / id

--- src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
+++ src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
@@ -2,11 +2,14 @@ package com.corsojava.pizzeria.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corsojava.pizzeria.model.Pizza;
 import com.corsojava.pizzeria.repo.PizzaRepo;
@@ -17,7 +20,7 @@ import com.corsojava.pizzeria.repo.PizzaRepo;
 @RequestMapping("/")
 public class PizzaController {
 
-    @Autowired
+	@Autowired
     PizzaRepo pizzaRepo;
 
 	@GetMapping
@@ -25,5 +28,29 @@ public class PizzaController {
 		List<Pizza> ListaPizze = pizzaRepo.findAll();
 		model.addAttribute("listaPizze", ListaPizze);
 		return "index";
+	}
+	
+	
+	
+	
+	@GetMapping("/details/{id}")		
+	public String details(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza=pizzaRepo.getReferenceById(id);
+		model.addAttribute("pizza", pizza);
+		return "details";
+	
+	}
+	
+//	PER FILTRARE LE PIZZE IN BASE AL NOME INSERITO DALL'UTENTE
+	@GetMapping("/cerca")
+	public String cerca(@RequestParam(name="nomePizza", required = false) String nomePizza, Model model) {
+	    List<Pizza> listaPizze;
+	    if (nomePizza != null) {
+	        listaPizze = pizzaRepo.findByNomeLike("%" + nomePizza + "%");
+	    } else {
+	        listaPizze = pizzaRepo.findAll();
+	    }
+	    model.addAttribute("listaPizze", listaPizze);
+	    return "index";
 	}
 }
--- src/main/java/com/corsojava/pizzeria/repo/PizzaRepo.java
+++ src/main/java/com/corsojava/pizzeria/repo/PizzaRepo.java
@@ -1,9 +1,13 @@
 package com.corsojava.pizzeria.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corsojava.pizzeria.model.Pizza;
 
 public interface PizzaRepo extends JpaRepository<Pizza, Integer> {
+
+	 List<Pizza> findByNomeLike(String nome);
 
 }
